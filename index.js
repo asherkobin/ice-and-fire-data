bf18076f7cef6e1343b0edb744137a3dcbacf681
Asher Kobin <asherkobin@gmail.com>
2020-02-23 22:09:09 -0500

    Test DB

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const server = express();
 const portNum = process.env.PORT || 5000;
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
+server.get("/", async (req, res) => {
+  const client = await pool.connect();
+  const result = await client.query("SELECT 'Asher Kobin' AS Name");
+
+  console.log("DB Result:", result);
+
   res.status(200).json("Hello World");
 })
 
